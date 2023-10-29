ba51ba8468d011d2bc83170f42cd51390bc0055b
LukaL <Lukalursmanashvili398@gmail.com>
2023-10-29 16:46:22 +0400

    Downloaded the code and uploaded it's data in firebase

---
+++ Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnCik51CinrKObEPqKhCOoVxki2y4Vkek",
+  authDomain: "react-notes-e6d6d.firebaseapp.com",
+  projectId: "react-notes-e6d6d",
+  storageBucket: "react-notes-e6d6d.appspot.com",
+  messagingSenderId: "637392726441",
+  appId: "1:637392726441:web:3ec00c1f255f19d25bfcb8"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+export const notesCollection = collection(db, 'notes')
